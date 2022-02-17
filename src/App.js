602a315f173ec6a68201233fd7858840d1ece942
cristianocsl <cslcristiano@gmail.com>
2022-02-17 17:06:33 -0300

    feat: adiciona rota de login em App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes ,Route } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/register" element={ <Register />} />
+          <Route path="/login" element={ <Login /> } />
         </Routes>
       </BrowserRouter>
       <Register />

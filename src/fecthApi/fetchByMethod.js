da2ecbfee16f1176adc23b1794753b4a6a96b6d1
cristianocsl <cslcristiano@gmail.com>
2022-02-17 17:02:56 -0300

    feat: adiciona chave de chamada de funçao de fetch para login

--- src/fecthApi/fetchByMethod.js
+++ src/fecthApi/fetchByMethod.js
@@ -1,9 +1,12 @@
+import fetchUserLogin from './fetchUserLogin';
 import fetchUserRegister from './fetchUserRegister';
+
 
 const fetchByMethod = async(method, content) => {
   
   const functionMap = {
     postRegister: await fetchUserRegister(content),
+    postLogin: await fetchUserLogin(content)
   };
   
   return functionMap[method];

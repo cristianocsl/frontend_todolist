5b51b62d399ac252083d8e3368483dc6996624ac
cristianocsl <cslcristiano@gmail.com>
2022-02-17 12:34:14 -0300

    fix: ajusta o retorno do erro da requisiçao

--- src/fecthApi/fetchUserRegister.js
+++ src/fecthApi/fetchUserRegister.js
@@ -4,11 +4,11 @@ const URL_API = 'https://backent-todolist-01.herokuapp.com/';
 
 const fetchUserRegister = async (userData) => {
   try {
-    const response = await axios.post(`${URL_API}/user`, userData);
+    const { data } = await axios.post(`${URL_API}user`, userData);
 
-    return response.data;
+    return data;
   } catch (error) {
-    return error.response.data;
+    return error.response.data.message;
   }
 };
 

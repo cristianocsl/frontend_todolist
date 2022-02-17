2df521516f976df7abd78fa5888d78cce2a2201a
cristianocsl <cslcristiano@gmail.com>
2022-02-17 13:42:53 -0300

    refactor: adiciona rota de register com react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes ,Route } from 'react-router-dom';
 import Register from './pages/Register';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={ <Register />} />
+        </Routes>
+      </BrowserRouter>
       <Register />
     </div>
   );

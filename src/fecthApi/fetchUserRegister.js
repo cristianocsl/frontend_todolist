1669a316843eb12ea4f7d4c024b0454bc42d4ae2
cristianocsl <cslcristiano@gmail.com>
2022-02-18 15:50:52 -0300

    feat: adiciona default para guardar token

--- src/fecthApi/fetchUserRegister.js
+++ src/fecthApi/fetchUserRegister.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const URL_API = 'https://backent-todolist-01.herokuapp.com/';
+const URL_API = 'http://backent-todolist-01.herokuapp.com/';
 
 const fetchUserRegister = async (userData) => {
   try {
     const { data } = await axios.post(`${URL_API}user`, userData);
-
+    axios.defaults.headers.common['Authorization'] = data.token;
     return data;
   } catch (error) {
     return error.response.data.message;

3e4708c4c8f33932caa1e2f2828ed2eef20fbd65
cristianocsl <cslcristiano@gmail.com>
2022-02-19 16:11:44 -0300

    style: adicona estilizaçao basica para campo de adicionar tarefa e botao de adicionar

---
+++ src/pages/TasksPage.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import TasksComponent from '../components/TasksComponent';
+import { TextField, IconButton } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import './taskPage.css';
+
+export default function TasksPage() {
+  const [tasks, setTasks] = useState([]);
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setTasks({
+      ...tasks,
+      [name]: value,
+    });
+  };
+
+  const handleAdd = async(event) => {
+    event.preventDefault();
+    setTasks({ ...tasks, task: '' });
+  };
+
+  return (
+    <div>
+      <div className="textfield-addbutton">
+        <TextField
+          sx={{ width: '100%' }}
+          className="content-field"
+          required
+          id="outlined-text-input"
+          type="text"
+          name="input"
+          value=""
+          onChange={ (e) => handleChange(e.currentTarget.value) }
+          />
+        <IconButton onClick={ handleAdd }>
+          <AddIcon />
+        </IconButton>
+      </div>
+      <TasksComponent />
+    </div>
+  );
+}

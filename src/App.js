a971ce1f977f96f5898a2fe7ee31b66407b6d808
cristianocsl <cslcristiano@gmail.com>
2022-02-19 00:43:55 -0300

    refactor: renomeia TasksPage.js em App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes ,Route, Navigate } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
-import Tasks from './pages/Tasks';
+import TasksPage from './pages/TasksPage';
 import './App.css';
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
           <Route path="/" element={ <Navigate replace to="/login" /> } />
           <Route path="/login" element={ <Login /> } />
           <Route path="/register" element={ <Register />} />
-          <Route path="/tasks" element={ <Tasks />} />
+          <Route path="/tasks" element={ <TasksPage />} />
         </Routes>
       </BrowserRouter>
     </div>

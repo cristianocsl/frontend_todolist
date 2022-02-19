52dfac6410e215f3e435df60943039dd6a74ea9c
cristianocsl <cslcristiano@gmail.com>
2022-02-19 16:05:03 -0300

    style: adiciona estilizaçao para cada card de tarefas

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -4,6 +4,7 @@ import { InputLabel, FormControl, Select, TextField, IconButton, MenuItem } from
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { shape, string, func } from 'prop-types';
+import './taskitem.css';
 
 export default function TaskItem({ item, sendChangesToFather }) {
   const [textValue, setTextValue] = useState(item.task);
@@ -27,44 +28,53 @@ export default function TaskItem({ item, sendChangesToFather }) {
     key={item._id + 1}
     className="container-task"
   >
-    <Box sx={{ minWidth: 110 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Status</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={item.status}
-          defaultValue={item.status}
-          name="select"
-          onChange={handleChangeSelect}
-        >
-          {
-            options.map((option) => {
-              return <MenuItem value={option} key={option}>{option}</MenuItem>;
-            })
-          }
-        </Select>
-      </FormControl>
-    </Box>
-    <TextField
-      key={item._id}
-      sx={{ width: '100%' }}
-      className="content-field"
-      required
-      id="outlined-text-input"
-      type="text"
-      name="text"
-      value={textValue}
-      onChange={handleChangeTextField}
-    />
-    <IconButton onClick={ handleDelete }>
-      <DeleteIcon />
-    </IconButton>
-    <IconButton onClick={ handleEdit }>
-      <EditIcon />
-    </IconButton>
-  </div>
+    <div className="date">
+      Criado em: { item.createdAt.split(' ')[0] }
+    </div>
 
+    <div className="select-edit-remove">
+      <Box className="select">
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Status</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={item.status}
+            defaultValue={item.status}
+            name="select"
+            onChange={handleChangeSelect}
+          >
+            {
+              options.map((option) => {
+                return <MenuItem value={option} key={option}>{option}</MenuItem>;
+              })
+            }
+          </Select>
+        </FormControl>
+      </Box>
+      <div className="buttons-container">
+        <IconButton onClick={ handleDelete } className="icon-button">
+          <DeleteIcon />
+        </IconButton>
+        <IconButton onClick={ handleEdit } className="icon-button">
+          <EditIcon />
+        </IconButton>
+      </div>
+    </div>
+    <div>
+      <TextField
+        key={item._id}
+        sx={{ width: '100%' }}
+        className="content-field"
+        required
+        id="outlined-text-input"
+        type="text"
+        name="text"
+        value={textValue}
+        onChange={handleChangeTextField}
+        />
+    </div>
+  </div>
   );
 }
 
@@ -74,6 +84,7 @@ TaskItem.propTypes = {
       name: string.isRequired,
       status: string.isRequired,
       task: string.isRequired,
+      createdAt: string.isRequired,
     }),
   sendChangesToFather: func.isRequired,
 };

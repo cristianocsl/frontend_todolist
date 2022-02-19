136c1698bc6e0090c8fa80f3c9a991285bbf0611
cristianocsl <cslcristiano@gmail.com>
2022-02-19 14:40:52 -0300

    feat: adiciona logica de mudar itens do estado do componente pai a partir do _id

--- src/components/TasksComponent.js
+++ src/components/TasksComponent.js
@@ -1,63 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { TextField, IconButton } from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
 import fetchByMethod from '../fecthApi';
+import TaskItem from './TaskItem';
 import './taskComponent.css';
 
 export default function TasksComponent() {
-  const [tasks, setTasks] = useState([{ id: 0, task: '' }]);
-
+  const [tasks, setTasks] = useState([]);
   
-  console.log('tasks ', tasks);
   useEffect(async () => {
     const data = await fetchByMethod.fetchAllTasks();
     setTasks(data);
   }, []);
 
-  const handleChange = ({ target }) => {
-    const { name, value } = target;
-    setTasks({
-      ...tasks,
-      [name]: value,
+  const handleChange = (taskObj) => {
+    const found = tasks.map((item) => {
+      if (item._id === taskObj._id) return {
+        ...item, ...taskObj
+      };
+      return item;
     });
+    setTasks(found);
   };
-
-  const handleDelete = () => {};
-
-  const handleEdit = () => {};
-
-  // const handleAdd = async(event) => {
-  //   event.preventDefault();
-  //   setTasks({ ...tasks, task: '' });
-  // };
 
   return (
     <div className="container">
       {
-        tasks.map((item) => (
-          <div
-            key={item._id + 1}
-            className="container-task"
-          >
-            <TextField
-              key={item._id}
-              sx={{ width: '100%' }}
-              className="content-field"
-              required
-              id="outlined-text-input"
-              type="text"
-              name="text"
-              value={item.task}
-              onChange={ (e) => handleChange(e.currentTarget.value, item._id) }
-            />
-            <IconButton onClick={ handleDelete }>
-              <DeleteIcon />
-            </IconButton>
-            <IconButton onClick={ handleEdit }>
-              <EditIcon />
-            </IconButton>
-          </div>
+        tasks.map((item, index) => (
+          <TaskItem item={item} key={index} sendChangesToFather={handleChange} />
         ))
       }
     </div>

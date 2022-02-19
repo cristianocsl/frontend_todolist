532cb068a7ec09b20971355e0ecf82db2bbcbd23
cristianocsl <cslcristiano@gmail.com>
2022-02-19 14:38:05 -0300

    refactor: organiza linhas do codigo

--- src/fecthApi/fetchTasks.js
+++ src/fecthApi/fetchTasks.js
@@ -5,10 +5,8 @@ const URL_API = 'http://backent-todolist-01.herokuapp.com/';
 const fetchTasks = async () => {
   const token = axios.defaults.headers.common['Authorization'];
 
-  
   try {
     const { data } = await axios.get(`${URL_API}task`, token);
-    console.log('data get >', data);
     return data;
   } catch (error) {
     return error.response.data.message;

6064e9283a79f0bb704b50a05d82b1890abac85c
cristianocsl <cslcristiano@gmail.com>
2022-02-20 00:10:08 -0300

    feat: cria funçao que faz post para criar tarefas

---
+++ src/fecthApi/fetchCreateTask.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const URL_API = 'http://backent-todolist-01.herokuapp.com';
+
+const fetchCreateTasks = async (task) => {
+  const token = axios.defaults.headers.common['Authorization'];
+
+  try {
+    const { data } = await axios.get(`${URL_API}/task`, task, token);
+
+    return data;
+  } catch (error) {
+    return error.response.data.message;
+  }
+};
+
+export default fetchCreateTasks;

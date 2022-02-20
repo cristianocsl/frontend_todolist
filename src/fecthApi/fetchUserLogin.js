413ff6490cc9f7684f483247e3d2b8ecaff8d04c
cristianocsl <cslcristiano@gmail.com>
2022-02-19 23:17:49 -0300

    feat: adiciona chave userName axios.defaults.headers['userName'] nas fetch de login e register

--- src/fecthApi/fetchUserLogin.js
+++ src/fecthApi/fetchUserLogin.js
@@ -7,6 +7,8 @@ const fetchUserLogin = async (userData) => {
   try {
     const { data } = await axios.post(`${URL_API}login`, userData);
     axios.defaults.headers.common['Authorization'] = data.token;
+    axios.defaults.headers['userName'] = data.name;
+
     return data;
   } catch (error) {
     return error.response.data.message;
--- src/fecthApi/fetchUserRegister.js
+++ src/fecthApi/fetchUserRegister.js
@@ -5,7 +5,10 @@ const URL_API = 'http://backent-todolist-01.herokuapp.com/';
 const fetchUserRegister = async (userData) => {
   try {
     const { data } = await axios.post(`${URL_API}user`, userData);
+    console.log('data', data);
     axios.defaults.headers.common['Authorization'] = data.token;
+    axios.defaults.headers['userName'] = data.name;
+
     return data;
   } catch (error) {
     return error.response.data.message;

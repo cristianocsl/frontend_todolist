a5944fbe10f4b7103399f4865aafe346fc73bc23
cristianocsl <cslcristiano@gmail.com>
2022-02-20 21:12:50 -0300

    feat: adiciona token no localstorage na pagina de login

--- src/fecthApi/fetchUserLogin.js
+++ src/fecthApi/fetchUserLogin.js
@@ -7,6 +7,7 @@ const fetchUserLogin = async (userData) => {
   try {
     const { data } = await axios.post(`${URL_API}login`, userData);
     axios.defaults.headers.common['Authorization'] = data.token;
+    localStorage.setItem('token', JSON.stringify(data.token));
     axios.defaults.headers['userName'] = data.name;
 
     return data;

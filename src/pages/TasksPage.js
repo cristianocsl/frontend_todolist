ca121677bb3bc63fe83c2a855b1a2a521b2de8fe
cristianocsl <cslcristiano@gmail.com>
2022-02-21 03:13:19 -0300

    style: adiciona logo e estilizaçao

--- src/pages/TasksPage.js
+++ src/pages/TasksPage.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import TasksComponent from '../components/TasksComponent';
+import { Box } from '@mui/material';
+import AddTaskIcon from '@mui/icons-material/AddTask';
+import { colors } from '@mui/material/';
 import axios from 'axios';
 
 import './taskPage.css';
@@ -7,8 +10,19 @@ import './taskPage.css';
 export default function TasksPage() {
   return (
     <div>
-      <h1>Olá, {axios.defaults.headers['userName']}!</h1>
-      <TasksComponent />
+      <div className="header-task-page">
+        <div className="add-task-logo">
+          <AddTaskIcon sx={{ fontSize: 25, color: colors.blue[900] }} />
+          <Box sx={{ fontSize: 20, color: colors.blue[600] }} >Add</Box>
+          <Box sx={{ fontSize: 15, color: colors.blue[300] }} >Task</Box>
+        </div>
+        <h3>
+          <Box sx={{ color: colors.blue[900] }}>
+            Olá, {axios.defaults.headers['userName']}!
+          </Box>
+        </h3>
+      </div>
+        <TasksComponent />
     </div>
   );
 }

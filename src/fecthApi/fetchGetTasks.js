16dd9f29da38953db7590345d04e61276b4a21b0
cristianocsl <cslcristiano@gmail.com>
2022-02-20 21:16:42 -0300

    feat: adiciona token no localstorage

--- src/fecthApi/fetchGetTasks.js
+++ src/fecthApi/fetchGetTasks.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const URL_API = 'http://backent-todolist-01.herokuapp.com/';
 
 const fetchGetTasks = async () => {
-  const token = axios.defaults.headers.common['Authorization'];
+  const tokenLocalStorage = JSON.parse(localStorage.getItem('token'));
+  const token = axios.defaults.headers.common['Authorization'] || tokenLocalStorage;
 
   try {
     const { data } = await axios.get(`${URL_API}task`, token);

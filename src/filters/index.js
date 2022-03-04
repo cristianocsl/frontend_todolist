b591122d2901e8ceaf6742df3ccff08b40807023
cristianocsl <cslcristiano@gmail.com>
2022-03-04 01:18:07 -0300

    feat: adiciona logica de ordenar por data

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,9 +1,20 @@
-const filterAlphabetically = (items) => {
+export const filterAlphabetically = (items) => {
   const array = items.sort((a, b) => {
     return (a.task > b.task) ? 1 : ((b.task > a.task) ? -1 : 0);
   });
-  
+
   return array;
 };
 
-export default filterAlphabetically;
+export const filterByDate = (items) => {
+  const array = items.sort((a, b) => {
+    return (a.createdAt > b.createdAt) ? -1 : ((b.createdAt > a.createdAt) ? 1 : 0);
+  });
+
+  return array;
+};
+
+export const filter = {
+  alphabetically: filterAlphabetically,
+  byDate: filterByDate,
+};

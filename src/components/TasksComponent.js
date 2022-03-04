3d80162139695091056f82f0f6c511256a47b1ff
cristianocsl <cslcristiano@gmail.com>
2022-03-04 19:29:40 -0300

    feat: adiciona funcionalidade de esconder/mostrar botoes de filtro

--- src/components/TasksComponent.js
+++ src/components/TasksComponent.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import fetchByMethod from '../fecthApi';
-import { TextField, IconButton } from '@mui/material';
+import { TextField, IconButton, Button } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import TaskItem from './TaskItem';
 import './taskComponent.css';
@@ -9,10 +9,18 @@ import { filter } from '../filters';
 import axios from 'axios';
 import { FaSortAlphaUp, FaSortAlphaUpAlt, FaSortNumericUp, FaSortNumericUpAlt } from 'react-icons/fa';
 import { BsCalendarDateFill  } from 'react-icons/bs';
+import { v4 as uuidv4 } from 'uuid';
 import FilterButton from './FilterButton';
+
+const PENDING = 'Pending';
+const TODO = 'To do';
+const DONE = 'Done';
+const ORIGINAL = 'Original';
 
 export default function TasksComponent() {
   const [tasks, setTasks] = useState([]);
+  const [copyTasks, setCopyTasks] = useState([]);
+  const [hideFilters, setShowFilters] = useState(true);
   const [textField, setTextField] = useState({ task: '' });
 
   const handleClickAtoZ = () => {
@@ -39,16 +47,22 @@ export default function TasksComponent() {
     setTasks(filtered);
   };
 
+  const handleClickStatus = (statusRef) => {
+    const tasksCopy = [...copyTasks];
+    const filtered = tasksCopy.filter(({ status }) => status === statusRef);
+    setTasks(filtered);
+  };
+
   const contentFilterButtons = [
     { text: <FaSortAlphaUp />, handleClick: handleClickAtoZ },
     { text: <FaSortAlphaUpAlt />, handleClick: handleClickZtoA },
     { text: <><FaSortNumericUp /> <BsCalendarDateFill /></>, handleClick: handleClickOldDate },
     { text: <><FaSortNumericUpAlt /> <BsCalendarDateFill /></>, handleClick: handleClickNewDate },
+    { text: PENDING, handleClick: () => handleClickStatus(PENDING), heightValue: '31px' },
+    { text: TODO, handleClick: () => handleClickStatus(TODO), heightValue: '31px' },
+    { text: DONE, handleClick: () => handleClickStatus(DONE), heightValue: '31px' },
+    { text: ORIGINAL, handleClick: () => setTasks(copyTasks), heightValue: '31px' },
   ];
-  
-  const handleClickStatus = () => {
-    
-  };
 
   const handleAddTask =  async (event) => {
     event.preventDefault();
@@ -63,6 +77,7 @@ export default function TasksComponent() {
     await setTokenInAxios(axios);
     const data = await fetchByMethod.fetchAllTasks();
     setTasks(data);
+    setCopyTasks(data);
   }, []);
 
   const handleChange = (taskObj) => {
@@ -101,24 +116,26 @@ export default function TasksComponent() {
         </IconButton>
       </form>
       <div>
-        <h3>
+        <Button
+          className="content-field"
+          variant="outlined"
+          size="large"
+          type="button"
+          sx={{ weight: '100%' }}
+          onClick={ () => setShowFilters((prevState) => !prevState) }
+        >
           Filtros
-        </h3>
+        </Button>
       </div>
-      <div>
+      <div hidden={hideFilters}>
         {
-          contentFilterButtons.map(({ text, handleClick}) => <FilterButton
-            key={text + 1}
+          contentFilterButtons.map(({ text, handleClick, heightValue}) => <FilterButton
+            key={ uuidv4() }
             text={ text }
             handleClick={ handleClick }
+            heightValue={ heightValue }
           />)
         }
-      </div>
-      <div>
-        <FilterButton
-          text="Pending"
-          handleClick={ handleClickStatus }
-        />
       </div>
       {
         tasks.map((item) => (

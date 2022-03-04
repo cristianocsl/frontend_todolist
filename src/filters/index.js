508df0c3dfce969ae392e4669169984a0fca8058
cristianocsl <cslcristiano@gmail.com>
2022-03-04 16:31:02 -0300

    refactor: renomeia funçoes de ordenaçao

--- src/filters/index.js
+++ src/filters/index.js
@@ -6,17 +6,20 @@ export const filterZtoA = (items) => items.sort((a, b) => {
     return (a.task > b.task) ? -1 : ((b.task > a.task) ? 1 : 0);
   });
 
-export const filterByDateUp = (items) => items.sort((a, b) => {
+export const filterByNewDate = (items) => items.sort((a, b) => {
     return (a.createdAt > b.createdAt) ? -1 : ((b.createdAt > a.createdAt) ? 1 : 0);
   });
 
-export const filterByDateDown = (items) => items.sort((a, b) => {
+export const filterByOldDate = (items) => items.sort((a, b) => {
     return (a.createdAt > b.createdAt) ? 1 : ((b.createdAt > a.createdAt) ? -1 : 0);
   });
+
+export const showByStatus = () => {};
 
 export const filter = {
   AtoZ: filterAtoZ,
   ZtoA: filterZtoA,
-  byDateUp: filterByDateUp,
-  byDateDown: filterByDateDown,
+  byNewDate: filterByNewDate,
+  byOldDate: filterByOldDate,
+  byStatus: showByStatus
 };

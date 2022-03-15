5c5ce56952e2da1cba31f8cb1e3093ed7cd9f485
cristianocsl <cslcristiano@gmail.com>
2022-03-15 01:52:30 -0300

    fix: remove texto adicional de boas vindas

--- src/pages/TasksHeader.js
+++ src/pages/TasksHeader.js
@@ -20,7 +20,7 @@ export default function TasksPage() {
         </div>
         <h3>
           <Box sx={{ color: colors.blue[900] }}>
-            Hello, { getUserName() }! You are welcome!
+            Hello, { getUserName() }!
           </Box>
         </h3>
       </div>

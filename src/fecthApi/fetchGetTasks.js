63ed45ff7f413f7c5e4eae9b54f59254d5a00814
cristianocsl <cslcristiano@gmail.com>
2022-03-15 02:22:21 -0300

    fix: remove console.log

--- src/fecthApi/fetchGetTasks.js
+++ src/fecthApi/fetchGetTasks.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 import { URL_API } from './urlApi';
 
 const fetchGetTasks = async () => {
-  // const tokenLocalStorage = JSON.parse(localStorage.getItem('token'));
-  // const token = axios.defaults.headers.common['Authorization'] || tokenLocalStorage;
-  // no momento desse try, a chave axios.defaults.headers.common['Authorization'] será reatribuída no useEffect do TasksComponentes.js com o token do localStorage.
   try {
     const { data } = await axios.get(`${URL_API}/task`);
 

d1902b2030c12759840a5d46b64bcad9a59b87f5
cristianocsl <cslcristiano@gmail.com>
2022-04-13 11:10:56 -0300

    refactor: refatora o arquivo de importação de funções fetch

--- src/fecthApi/index.js
+++ src/fecthApi/index.js
@@ -1,3 +1,20 @@
-import fetchByMethod from './fetchByMethod';
+import fetchCreateTask from './fetchCreateTask';
+import fetchGetTasks from './fetchGetTasks';
+import fetchUpdateTask from './fetchUpdateTask';
+import fetchUserLogin from './fetchUserLogin';
+import fetchUserRegister from './fetchUserRegister';
+import fetchRemoveTask from './fetchRemoveTask';
+import fetchWakeup from './fetchWakeup';
+
+
+const fetchByMethod = {
+    postRegister: fetchUserRegister,
+    loginPost: fetchUserLogin,
+    fetchAllTasks: fetchGetTasks,
+    fetchCreateTask,
+    fetchUpdateTask,
+    fetchRemoveTask,
+    fetchWakeup,
+};
 
 export default fetchByMethod;
